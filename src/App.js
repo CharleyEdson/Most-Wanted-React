139f5309507ca9f6fd053759c38e65a3a6bea5ed
Charley <charley1135@gmail.com>
2022-11-17 14:50:08 -0800

    feat: Created Table. Created filter function to search by person.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {DATA} from '../src/data'
+//Component Imports
+import NavBar from "./Components/NavBar/NavBar";
+import Table from './Components/Table/Table';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const[data,setData] = useState(DATA)
+
+  return <div className="App">
+    <NavBar />
+    <Table data={data}/>
+  </div>;
 }
 
 export default App;
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+const NavBar = (props) => {
+    return ( 
+    <div className ='nav'>
+    <h1>Most Wanted React 😎</h1>
+  </div> );
+}
+ 
+export default NavBar;
